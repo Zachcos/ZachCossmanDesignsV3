93357eb4221c75d69adddeec0523fae4d7177299
Zach Cossman <Zachcos@gmail.com>
2019-01-06 15:59:58 -0500

    Add first styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
+import styled from "styled-components";
 
-export default () => <div>Hello world!</div>
+const HeroWrapper = styled.div`
+  background: darkred;
+  width: 100%;
+  height: 250px;
+  margin: 0;
+  padding: 0;
+  p {
+    margin: 0;
+    padding: 0;
+    color: white;
+  }
+`;
+
+export default () => (
+  <HeroWrapper>
+    <p>this is a test</p>
+  </HeroWrapper>
+);
